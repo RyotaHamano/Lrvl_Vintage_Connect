bc8ba504e61bb913dd66c9405fc6c525823f0c65
EC2 Default User <ec2-user@ip-172-31-18-48.ap-northeast-1.compute.internal>
2023-10-27 10:38:40 +0000

    feat:tags/post_tagsテーブル記述

--- database/migrations/2023_10_27_072612_create_tags_table.php
+++ database/migrations/2023_10_27_072612_create_tags_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
     {
         Schema::create('tags', function (Blueprint $table) {
             $table->id();
+            $table->string('name', 20);
+            $table->boolean('is_available');
+            $table->foreignId('user_id');
             $table->timestamps();
         });
     }
--- database/migrations/2023_10_27_072747_create_post_tags_table.php
+++ database/migrations/2023_10_27_072747_create_post_tags_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
     {
         Schema::create('post_tags', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('post_item_id')->constrained('post_items')->onDelete('cascade');
+            $table->foreignId('tag_id')->constrained('tags')->onDelete('cascade');
             $table->timestamps();
         });
     }
