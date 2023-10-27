d93d09bb7f847b3394177ad0f579f8d6bf7a4e8f
EC2 Default User <ec2-user@ip-172-31-18-48.ap-northeast-1.compute.internal>
2023-10-27 09:45:44 +0000

    feat:post_itemsテーブル記述

---
+++ database/migrations/2023_10_27_072531_create_post_items_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('post_items', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->string('prefecture');
+            $table->string('address');
+            $table->string('shop_name', 20);
+            $table->string('shop_genre');
+            $table->text('review', 400);
+            $table->integer('rate');
+            $table->boolean('reading_status');
+            $table->foreignId('user_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('post_items');
+    }
+};
